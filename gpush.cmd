ff22ac06662d882d42548c959e560a851308a626
Sykie Chen <sykiechen@gmail.com>
2015-05-22 14:52:22 +0800

    pics not in center

--- gpush.cmd
+++ gpush.cmd
@@ -4,5 +4,5 @@ set /p cmt="Please enter commit text, leave empty to add time: "
 if defined cmt (
 git commit -m "%cmt%"
 ) else git commit -m "%date%%time%"
-git push -u origin master
+git push -u origin hic17
 pause
