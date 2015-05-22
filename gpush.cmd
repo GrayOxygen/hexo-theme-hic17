bbc3c5d412e1126f2423ac23f9b5720c704b6952
Sykie Chen <sykiechen@gmail.com>
2015-05-22 14:51:39 +0800

    pictures not in center

--- gpush.cmd
+++ gpush.cmd
@@ -1,8 +1,8 @@
 @echo off
 git add . --all
-set /p cmt="Please enter commit text,leave empty to add time: "
-if defined str (
-git commit -m %cmt%
+set /p cmt="Please enter commit text, leave empty to add time: "
+if defined cmt (
+git commit -m "%cmt%"
 ) else git commit -m "%date%%time%"
 git push -u origin master
 pause
